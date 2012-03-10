d27d0bfd892d20ee40cb0266552207e214cab4af
Patrick Labbett <patrick.labbett@gmail.com>
2012-03-10 01:59:54 -0500

    added SubmitClientMessage and ClientQuery methods and documentation

--- wctp.class.php
+++ wctp.class.php
@@ -19,19 +19,104 @@
 		*@param url	URL for the WCTP carrier gateway endpoint (required)
 		*/
 		public function __construct($url = false){
-			if(!$url){
-				die('Missing required parameter $url');
-			}
+			if(!$url){ die('Missing required parameter $url'); }
 			else{
 				$this->carrier['url'] = $url;
 			}
 		}
 		
-		
-		public function SubmitClientMessage(){
+		/**
+		*Initiate a message from a transient client to a carrier gateway
+		*
+		*@param senderID Phone number of sending device (required)
+		*@param recipientID Phone number of device message was sent to (required)
+		*@param message Alphanumeric message to send (required)
+		*@param miscInfo Misc info field, some carriers use this for a passcode field (optional)
+		*@param authorizationCode Required by some carriers
+		*@param msgControl Array of message control options from WCTP specification
+		*	sendResponsesToID         CDATA  #IMPLIED 
+		*	allowResponse             ( true | false ) "true" 
+		*	notifyWhenQueued          ( true | false ) "false" 
+		*	notifyWhenDelivered       ( true | false ) "false" 
+		*	notifyWhenRead            ( true | false ) "false" 
+		*	deliveryPriority          ( HIGH | NORMAL | LOW) "NORMAL" 
+		*	deliveryAfter             CDATA  #IMPLIED 
+		*	deliveryBefore            CDATA  #IMPLIED 
+		*	preformatted              ( true | false ) "false" 
+		*	allowTruncation           ( true | false ) "true"
+		*/
+		public function SubmitClientMessage($senderID = false, $recipientID = false, $message = false, $miscInfo = false, $authorizationCode = false, $msgControl = false){
+			if(!$senderID){	die('Missing required parameter $senderID.'); }
+			if(!$recipientID){	die('Missing required parameter $recipientID.'); }
+			if(!$message){	die('Missing required parameter $message.'); }
+  
+			$xml = '<!DOCTYPE wctp-Operation SYSTEM "http://dtd.wctp.org/wctp-dtd-v1r1.dtd">';
+			$xml .= '<?xml version="1.0" encoding="utf-8" ?>';
+			$xml .= '<wctp-Operation>';
+			$xml .= '<wctp-SubmitClientMessage>';
+			$xml .= '<wctp-SubmitClientHeader>';
+			$xml .= '<wctp-ClientOriginator senderID="' . $senderID;
+			if(!$miscInfo){
+			}
+			else{
+				$xml .= '" miscInfo="' . $miscInfo . '" />';
+			}
+
+			if(!$msgControl){
+			}
+			else{
+				foreach($msgControl as $option => $value){
+					$messageControl .=  $option . '="' . $value . '" ';
+				}
+			
+				$xml .= '<wctp-MessageControl ' . $messageControl . ' />';
+			}
+			$xml .= '<wctp-Recipient recipientID="' . $recipientID;
+			if(!authorizationCode){
+			}
+			else{
+				$xml .= '" authorizationCode="' . $authorizationCode . '" />';
+			}
+			$xml .= '</wctp-SubmitClientHeader>';
+			$xml .= '<wctp-Payload>';
+			$xml .= '<wctp-Alphanumeric>';
+			$xml .= $message;
+			$xml .= '</wctp-Alphanumeric>';
+			$xml .= '</wctp-Payload>';
+			$xml .= '</wctp-SubmitClientMessage>';
+			$xml .= '</wctp-Operation>';
+            
+            
+			$response = $this->http_post_xml($xml);
+			$xml_object = simplexml_load_string($response);
+			$this->carrier['response'] = '';
+		 	$this->parse_xml($xml_object);
 		}
 
-		public function ClientQuery(){
+		/**
+		*Poll operation to check for status information or responses to a message submitted by SubmitClientMessage
+		*
+		*@param senderID Phone number of sending device (required)
+		*@param recipientID Phone number of device message was sent to (required)
+		*@param trackingNumber Tracking number recieved from response of successful SubmitClientMessage (required)
+		*/
+		public function ClientQuery($senderID = false, $recipientID = false, $trackingNumber = false){
+			if(!$senderID){	die('Missing required parameter $senderID.'); }
+			if(!$recipientID){	die('Missing required parameter $recipientID.'); }
+			if(!$trackingNumber){	die('Missing required parameter $trackingNumber.'); }
+			
+			$xml = '<!DOCTYPE wctp-Operation SYSTEM "http://dtd.wctp.org/wctp-dtd-v1r1.dtd">';
+			$xml .= '<?xml version="1.0" encoding="utf-8" ?>';
+			$xml .= '<wctp-Operation>';
+			$xml .= '<wctp-ClientQuery senderID="' . $senderID . 
+				'" recipientID="' . $recipientID . 
+				'" trackingNumber="' . $trackingNumber . '" />';
+			$xml .= '</wctp-Operation>';
+            
+			$response = $this->http_post_xml($xml);
+			$xml_object = simplexml_load_string($response);
+			$this->carrier['response'] = '';
+		 	$this->parse_xml($xml_object);
 		}
 
 		/**
@@ -55,10 +140,10 @@
 			}
 			$xml .= "</wctp-Operation>";
 
-				$response = $this->http_post_xml($xml);
-				$xml_object = simplexml_load_string($response);
-				$this->carrier['response'] = '';
-			 	$this->parse_xml($xml_object);
+			$response = $this->http_post_xml($xml);
+			$xml_object = simplexml_load_string($response);
+			$this->carrier['response'] = '';
+		 	$this->parse_xml($xml_object);
 			
 		}
 
@@ -106,8 +191,8 @@
 		}
 	}
 
-	$tc = new WCTP_Transient_Client('https://wctp.att.net/wctp'); //Initialize our Transient Client for use with AT&T
-	$tc->VersionQuery('6143704609', mktime());
-	print_r($tc->carrier['response']);
+	//$tc = new WCTP_Transient_Client('https://wctp.att.net/wctp'); //Initialize our Transient Client for use with AT&T
+	//$tc->VersionQuery('6145551234', mktime());
+	//print_r($tc->carrier['response']);
 
 ?>
